fe4db84ba72fd273c8b8789b8f9a09edaff47f6a
JonaSimonDaginnus <jona.daginnus@student.uni-siegen.de>
2022-07-16 23:36:31 +0200

    modul table und style.css erstellt

---
+++ uni/src/components/ModulTable.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../styling'
+
+const ModulTable = () => {
+  return (
+    <table id="tabelle1" border = "1">
+    <thead>
+        <tr>
+            <th>1.Semester</th>
+            <th>2.Semester</th>
+            <th>3.Semester</th>
+            <th>4.Semester</th>
+            <th>5.Semester</th>
+            <th>6.Semester</th>
+            <th>7.Semester</th>
+            <th>8.Semester</th>
+            <th>9.Semester</th>
+            <th>10.Semester</th>
+            <th>11.Semester</th>
+            <th>12.Semester</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+            <td>Table Data</td>
+        </tr>
+    </tbody>
+</table>
+  )
+}
+
+export default ModulTable
